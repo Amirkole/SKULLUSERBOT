c196a018211799e7cafd4dbec569a4b7d3b5cf23
hackelite01 <70997872+hackelite01@users.noreply.github.com>
2020-12-19 17:40:01 +0530

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,94 @@
+# sourcery skip: de-morgan
+import os
+import sys
+import time
+from distutils.util import strtobool as sb
+from logging import DEBUG, INFO, basicConfig, getLogger
+
+import heroku3
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+from .Config import Config
+
+StartTime = time.time()
+Skullversion = "2.9.4"
+
+if Config.STRING_SESSION:
+    session_name = str(Config.STRING_SESSION)
+    if session_name.endswith("="):
+        bot = TelegramClient(
+            StringSession(session_name), Config.APP_ID, Config.API_HASH
+        )
+    else:
+        bot = TelegramClient(
+            "TG_BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
+        ).start(bot_token=Config.STRING_SESSION)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
+
+# PaperPlaneExtended Support Configs
+ENV = os.environ.get("ENV", False)
+
+SKUKK_ID = ["997887920", "1291824841"]
+
+# Bot Logs setup:
+if bool(ENV):
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=DEBUG,
+        )
+    else:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
+        )
+    LOGS = getLogger(__name__)
+
+    # Check if the config was edited by using the already used Configiable.
+    # Basically, its the 'virginity check' for the config file ;)
+    CONFIG_CHECK = os.environ.get(
+        "___________PLOX_______REMOVE_____THIS_____LINE__________", None
+    )
+    if CONFIG_CHECK:
+        LOGS.info(
+            "Please remove the line mentioned in the first hashtag from the config.env file"
+        )
+        quit(1)
+    try:
+        if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+            HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+                Config.HEROKU_APP_NAME
+            ]
+        else:
+            HEROKU_APP = None
+    except:
+        HEROKU_APP = None
+
+else:
+    # Put your ppe Configs here if you are using local hosting
+    PLACEHOLDER = None
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# showing imports error
+
+from .helpers import *
+from .helpers import functions as catdef
